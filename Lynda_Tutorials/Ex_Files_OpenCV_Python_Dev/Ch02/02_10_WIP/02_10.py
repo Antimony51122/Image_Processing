b67954dd0e296c7b09b361eadbf92ccdc28abe06
Antimony51122 <zhuyaosheng51122@gmail.com>
2018-02-14 14:33:15 +0000

    simple drawing app challenge

--- Lynda_Tutorials/Ex_Files_OpenCV_Python_Dev/Ch02/02_10_WIP/02_10.py
+++ Lynda_Tutorials/Ex_Files_OpenCV_Python_Dev/Ch02/02_10_WIP/02_10.py
@@ -2,17 +2,23 @@ import numpy as np
 import cv2
 
 # Global variables
-canvas = np.ones([500,500,3],'uint8')*255
+canvas = np.ones([500, 500, 3], 'uint8')*255
+radius = 3
+colour = (0, 255, 0)
+pressed = False  # to know whether the key is pressed or not
 
 # click callback
 def click(event, x, y, flags, param):
-	global canvas
-	if event == cv2.EVENT_LBUTTONDOWN:
-		print("LButton Down")
-	elif event == cv2.EVENT_MOUSEMOVE:
-		print("Mouse Move")
-	elif event == cv2.EVENT_LBUTTONUP:
-		print("LButton Up")
+    global canvas, pressed
+    if event == cv2.EVENT_LBUTTONDOWN:
+        pressed = True
+        cv2.circle(canvas, (x, y), radius, colour, -1)
+    elif event == cv2.EVENT_MOUSEMOVE and pressed == True:
+        cv2.circle(canvas, (x, y), radius, colour, -1)
+    elif event == cv2.EVENT_LBUTTONUP:
+        pressed = False
+
+# we want to draw circles when the EVENT_LBUTTONDOWN is triggered
 
 # window initialization and callback assignment
 cv2.namedWindow("canvas")
@@ -21,12 +27,15 @@ cv2.setMouseCallback("canvas", click)
 # Forever draw loop
 while True:
 
-	cv2.imshow("canvas",canvas)
+    cv2.imshow("canvas", canvas)
 
-	# key capture every 1ms
-	ch = cv2.waitKey(1)
-	if ch & 0xFF == ord('q'):
-		break
-	
+    # key capture every 1ms
+    ch = cv2.waitKey(1)
+    if ch & 0xFF == ord('q'):
+        break
+    elif ch & 0xFF == ord('b'):
+        colour = (255, 0, 0)
+    elif ch & 0xFF == ord('g'):
+        colour = (0, 255, 0)
 
 cv2.destroyAllWindows()
