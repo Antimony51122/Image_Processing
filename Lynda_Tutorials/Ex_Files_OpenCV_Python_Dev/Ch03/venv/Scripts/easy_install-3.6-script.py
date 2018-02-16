0accb3f1bd506426cfbd734f9376ef1b65498e66
Huang <zhuyaosheng51122@gmail.com>
2018-02-16 09:03:08 +0000

    contour object detection

---
+++ Lynda_Tutorials/Ex_Files_OpenCV_Python_Dev/Ch03/venv/Scripts/easy_install-3.6-script.py
@@ -0,0 +1,12 @@
+#!C:\Users\rh3014\coding\Image_Processing\Lynda_Tutorials\Ex_Files_OpenCV_Python_Dev\Ch03\venv\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==28.8.0','console_scripts','easy_install-3.6'
+__requires__ = 'setuptools==28.8.0'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('setuptools==28.8.0', 'console_scripts', 'easy_install-3.6')()
+    )
