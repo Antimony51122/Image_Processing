66f4c0ce830da0d51a2a0205d59314f83cb06500
Antimony51122 <zhuyaosheng51122@gmail.com>
2018-03-15 09:10:55 +0000

    robotcis session 3.15

--- final_demo/ROS_imgProcessing_subscribePublish.py
+++ final_demo/ROS_imgProcessing_subscribePublish.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from __future__ import print_function
 
 # import roslib
@@ -28,8 +27,8 @@ from data import BaseTransform, VOC_CLASSES as labelmap
 from ssd import build_ssd
 import imageio
 
-
-
+"""from Lynda tutorial"""
+import random
 
 class image_converter:
 
@@ -61,13 +60,39 @@ class image_converter:
         # if cols > 60 and rows > 60 :
         #     cv2.circle(cv_image, (50, 50), 10, 255)
 
+        gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+        blur = cv2.GaussianBlur(gray, (3, 3), 0)
 
+        thresh = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 205, 1)
+        # inversing the threshold because the foreground is white and we want to take out the objects which are going to be darker than this colour
+        cv2.imshow("Binary", thresh)
+
+        _, contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        # underscore to not use the first return
+        print(len(contours))
+
+        filtered = []
+        for c in contours:
+            if cv2.contourArea(c) < 1000: continue
+            filtered.append(c)
+
+        print(len(filtered))
+
+        objects = np.zeros([cv_image.shape[0], cv_image.shape[1], 3], 'uint8')
+        for c in filtered:
+            col = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+            cv2.drawContours(objects, [c], -1, col, -1)
+            area = cv2.contourArea(c)
+            p = cv2.arcLength(c, True)
+            print(area, p)
+
+        cv2.imshow("Contours", objects)
 
         cv2.waitKey(3)
 
         """convert OpenCV to ROS image and publish"""
         try:
-            self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
+            self.image_pub.publish(self.bridge.cv2_to_imgmsg(objects, "bgr8"))
             print('cv image has been published')
         except CvBridgeError as e:
             print(e)
